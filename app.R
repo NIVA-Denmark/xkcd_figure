acae1b0fed444a59125e084d011f354cb48a2d01
ciaran-dk <ciaran@murraynet.dk>
2018-09-23 20:24:24 +0200

    work

---
+++ app.R
@@ -0,0 +1,24 @@
+library(shiny)
+library(extrafont)
+
+# Define UI for dataset viewer app ----
+ui <- basicPage(
+  
+  verbatimTextOutput("fontlist")
+)
+
+# Define server logic to summarize and view selected dataset ----
+server <- function(input, output) {
+  
+  font_import(paths=c("./"),prompt=F)   ## because we downloaded to working directory
+  loadfonts()
+  
+  
+  flist<-paste(fonts(),collapse = "\n")
+  #flist<-paste(fonts(),sep="\n")
+  output$fontlist <- renderText({ flist })
+  
+}
+
+# Create Shiny app ----
+shinyApp(ui = ui, server = server)
